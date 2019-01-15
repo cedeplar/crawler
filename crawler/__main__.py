7e0ce0910e60ba930fdc4f7d32b5c4e47b53e6d4
Marcelo Duarte Trevisani <marceloduartetrevisani@gmail.com>
2019-01-15 15:49:38 -0200

    Using ui files to generate the interface.
    Add cedeplar logo

--- crawler/__main__.py
+++ crawler/__main__.py
@@ -4,6 +4,7 @@ from PyQt5.QtWidgets import QApplication
 from crawler.view.main_window import MainWindow
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
+    app = QApplication([])
     gui = MainWindow()
+    gui.show()
     sys.exit(app.exec_())
--- crawler/view/main_window.py
+++ crawler/view/main_window.py
@@ -1,15 +1,11 @@
-from pathlib import Path
-
-from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QMainWindow
+
+from crawler.view.ui.mainWindow_ui import Ui_MainWindow
 
 
 class MainWindow(QMainWindow):
 
     def __init__(self):
         super(MainWindow, self).__init__()
-        self.setWindowTitle("CEDEPLAR - WebCrawler - UFMG")
-
-        cedeplar_win_logo = Path('icons', 'cedeplar_ufmg_small.png')
-        icon_window = QIcon(str(cedeplar_win_logo))
-        self.setWindowIcon(icon_window)
+        self._gui = Ui_MainWindow()
+        self._gui.setupUi(self)
